66b3f2019273f828e835c3c896cc6612780b35cb
Adrian Ziętek <adrian.kamil.zietek@gmail.com>
2025-06-22 20:22:21 +0200

    Add script for csv conversion

---
+++ scripts/csv_konwersja.py
@@ -0,0 +1,13 @@
+input_file = "data/rule_based_scores.csv"
+output_file = "data/rule_based_scores_po_konwersji.csv"
+
+with open(input_file, "r", encoding="utf-8") as f:
+    content = f.read()
+
+content = content.replace(",", ".")
+content = content.replace(";", ",")
+
+with open(output_file, "w", encoding="utf-8") as f:
+    f.write(content)
+
+print("Zamiana zakończona. Wynik zapisano w", output_file)
