86e9bbf8089ef2623e74e3e88b129bbaa035fe2b
Adrian Ziętek <adrian.kamil.zietek@gmail.com>
2025-06-22 20:22:35 +0200

    Add script for creating datasets

---
+++ scripts/huggingface_datasets.py
@@ -0,0 +1,21 @@
+from datasets import load_dataset, DatasetDict
+import os
+
+# Ścieżka do JSONL
+data_path = 'processed/merged_dataset.jsonl'
+
+# Wczytanie jako dataset
+dataset = load_dataset('json', data_files=data_path, split='train')
+
+# Podział na train/val (np. 80/20)
+dataset = dataset.train_test_split(test_size=0.2, seed=42)
+dataset_dict = DatasetDict({
+    'train': dataset['train'],
+    'validation': dataset['test']
+})
+
+# Zapis do katalogu
+output_dir = 'processed/hf_dataset'
+dataset_dict.save_to_disk(output_dir)
+
+print(f'Dataset zapisany do {output_dir}')
