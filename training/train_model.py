ea3d0b356ad187ef32987f34feca7b8053e9836f
Adrian Ziętek <adrian.kamil.zietek@gmail.com>
2025-06-27 16:31:17 +0200

    First attempt to model training

--- training/train_model.py
+++ training/train_model.py
@@ -1,4 +1,4 @@
-from transformers import LongformerForSequenceClassification, LongformerTokenizerFast, TrainingArguments, Trainer, LongformerConfig, TrainingArguments
+from transformers import LongformerForSequenceClassification, LongformerTokenizerFast, TrainingArguments, Trainer, LongformerConfig, default_data_collator
 from datasets import load_from_disk
 import json
 from datetime import datetime
@@ -14,37 +14,32 @@ output_dir = os.path.join(cfg["model_output_path"], f"longformer-{timestamp}")
 # Wczytaj tokenizowany dataset z dysku
 dataset = load_from_disk(cfg["tokenizer_output_path"])
 
-# Konfiguracja modelu (multi-label classification)
+# Konfiguracja modelu
 config = LongformerConfig.from_pretrained(
     cfg["model_name"],
     num_labels=cfg["num_labels"],
     problem_type=cfg["problem_type"]
 )
 
-# Wczytaj model Longformer
+# Wczytaj model
 model = LongformerForSequenceClassification.from_pretrained(
     cfg["model_name"],
-    config=config,
-    torch_dtype="auto",
-    device_map="auto"
+    config=config
 )
 
 # Argumenty treningowe
 args_cfg = cfg["training_args"]
 training_args = TrainingArguments(**args_cfg)
 
-# Stworzenie trenera
+# Dodaj default_data_collator
 trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=dataset["train"],
     eval_dataset=dataset["validation"],
+    data_collator=default_data_collator
 )
 
-# Trening modelu
 trainer.train()
-
-# Zapis wytrenowanego modelu
 trainer.save_model(output_dir)
-
 print("✅ Trening zakończony. Model zapisany.")
